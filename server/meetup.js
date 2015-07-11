87a19cc4be2f56fff2d77958a6863334a3a0927f
Jorge Barrachina <nusquema@gmail.com>
2015-07-11 15:34:57 +0200

    Fixed Typo on Roles

--- server/meetup.js
+++ server/meetup.js
@@ -22,7 +22,7 @@ Accounts.onCreateUser(function(options, user){
         return {
           name : prof.name,
           photo : prof.photo_url,
-          role : _.contains(["Organizer","Assistant Organizer","Co-organizer","Event Organizer"],prof.role) ? "admin" : "member"
+          role : _.contains(["Organizer","Assistant Organizer","Co-Organizer","Event Organizer"],prof.role) ? "admin" : "member"
         };
       })[0];
 
